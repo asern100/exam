f83976873c55a982ab70b277f2894fb922bf8df4
asern100 <saiefeddina@gmail.com>
2021-12-05 23:48:38 +0100

    upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { questions } from './Question';
+export default function App() {
+	
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					تحصلت على {score} من {questions.length}
+				</div>
+			) : (
+				<>
+					
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>السؤال {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
---
+++ src/Question.js
@@ -0,0 +1,204 @@
+export const questions =  
+[
+    {
+        questionText: 'يجب مسح جميع الرأس في الوضوء',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    
+    {
+        questionText: 'إذا كان الشعر مُسْترخِيا (طويلا) فيجب مسحه',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'المرفق والكعبان غير  داخلين فيما يجب غسله',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'الدلك واجب في الوضوء بباطن اليد فقط.',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'من غسل رجليه في الوضوء بِدَلْك بعضهما ببعض فوضوؤه باطل',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'غسل الرأس في الوضوء مُـجْزِيء عن مَسْحِه',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'بعدما غسل وجهه ردَّ على الهاتف  وجَفَّ وجهه، ثم تابع الوضوء بعد الوجه',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'معيار جفاف الأعضاء هو طبيعة الرجل معتدل الحرارة في زمن معتدل',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'المسحة الأولى للرأس سنة والثانية فريضة',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'تجديد الماء للأذنين سنة مستقلة عن مسح الأذنين',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'إذا لم يرتِّب فرائض الوضوء صح وضوؤه وفاته أجر السُّنة',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'من نسي غسل فرضٍ وصلى بذلك الوضوء يجب إعادة الصلاة بعد غسل الفرض',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'إذا تذكر الفرض المنسِيّ قبل جفاف الأعضاء، أعاده ولم يبدأ الوضوء من جديد',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'إذا شرع في غسل الوجه ناسيا المضمضة فلا يعود للمضمضة والاستنشاق',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'من صلى ناسيا سنة من سنن الوضوء لم يعد تلك الصلاة',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'الغسلة الثانية والثالثة في الوضوء فضيلة',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'السواك من سنن الوضوء',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'البُداءة بمسح الرأس من مقدمه فضيلة',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'حكم تخليل أصابع القدمين فضيلة، يصح الوضوء دونها، ولو لَـم يصلها الماء',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'يجب تخليل أصابع اليدين',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'يجب تخليل اللحية الخفيفة التي تظهر البشرة من تحته',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'يجب تخليل اللحية الكثيفة التي لا تظهر البشرة من تحتها في الوضوء',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'يجب تخليل اللحية الكثيفة في الغسل',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'يبطل الوضوء إذا ترك التسمية في أوله',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: false },
+            { answerText: 'خطأ', isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'حكم الاستنثار (إخراج الماء من الأنف بقوة النفس) فضيلة',
+        answerOptions: [
+            { answerText: 'صح', isCorrect: true },
+            { answerText: 'خطأ', isCorrect: false },
+            
+        ],
+    },
+];
