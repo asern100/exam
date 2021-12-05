0624dba1453d4e2209e3e18c5d3bcc428ec5057d
asern100 <saiefeddina@gmail.com>
2021-12-06 00:24:40 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -28,16 +28,17 @@ export default function App() {
 			) : (
 				<>
 					
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
+					
 					<div className='question-section'>
 						<div className='question-count'>
 							<span>السؤال {currentQuestion + 1}</span>/{questions.length}
 						</div>
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
 					</div>
 				</>
 			)}
